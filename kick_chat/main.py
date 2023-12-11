5d584ca48a3c8b9a60b75a9175a0369ebbe74dc6
baemincheon <baemincheon@gmail.com>
2023-12-11 23:57:39 +0900

    Copy the cert file only when it runs as frozen, which means that the user is using an executable (from PyInstaller).

--- kick_chat/main.py
+++ kick_chat/main.py
@@ -4,17 +4,30 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from kick_chat.client import Client
+from client import Client
 
+def copy_cert():
+    execution_path = os.path.dirname(sys.argv[0])
+    cert_path = os.path.join(execution_path , 'cacert.pem')
+    mei_path = os.path.dirname(__file__)
+    cffi_path = os.path.join(mei_path, "curl_cffi")
+    if not os.path.exists(cffi_path):
+        os.makedirs(cffi_path)
+    new_path = os.path.join(cffi_path, "cacert.pem")
+    import shutil
+    shutil.copyfile(cert_path, new_path)
+    print("original_cert_path: " + cert_path)
+    print("copied_cert_path: " + new_path)
 
 def main(argv=sys.argv) -> int:
     if len(argv) != 2:
         print("Usage: kick-chat <channel_name>")
         return 1
+    if getattr( sys , 'frozen' , None ):
+        copy_cert()
     client = Client(username=argv[1])
     client.listen()
     return 0
-
 
 if __name__ == "__main__":
     raise SystemExit(main())
